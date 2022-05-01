4fb1d7c5bdd28e4a3391208cb17336a4e03d231f
Tempest1000 <gritstone1000@gmail.com>
2022-05-01 16:37:56 -0500

    Initial commit

---
+++ src/test/java/org/springframework/samples/petclinic/coverage/CoverageTests.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.coverage;
+
+import org.junit.jupiter.api.Test;
+
+public class CoverageTests {
+
+	@Test
+	void parseCoverageReport() {
+		System.out.println("parseCoverageReport");
+	}
+}
